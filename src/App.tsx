f4b41778b7a8e8271db5b94015ec0d86fd968fc4
Adisingh07 <88798791+Adisingh07@users.noreply.github.com>
2025-05-16 00:08:49 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import SearchPage from './Shop/index';
 import ResultsPage from './Shop/ResultsPage';
+import FeedbackForm from './Shop/components/feedback';
+import ContactUs from './Shop/components/contact';
+import Roadmap from './Shop/components/roadmap';
+import PrivacyPolicy from './Shop/components/privacy';
+import Terms from './Shop/components/terms';
+
+
 
 function App() {
   return (
@@ -9,6 +16,11 @@ function App() {
       <Switch>
         <Route exact path="/" component={SearchPage} />
         <Route path="/results" component={ResultsPage} />
+        <Route path="/feedback" component={FeedbackForm} />
+        <Route path="/contact" component={ContactUs} />
+        <Route path="/roadmap" component={Roadmap} />
+        <Route path="/privacy" component={PrivacyPolicy} />
+        <Route path="/terms" component={Terms} />
       </Switch>
     </Router>
   );
